e96da0bff4d5dbae1f8d5d6ef0a468c864763840
chris <tanchongtao@stosz.com>
2018-05-07 16:56:58 +0800

    update

--- libs/libs.swws.php
+++ libs/libs.swws.php
@@ -145,7 +145,7 @@ class Libs_SwWs {
     public function onOpen($ws, $request)
     {
         foreach ($ws->ports as $ports) {
-            print_r($ports);
+            print_r($ports->port);
         }
         Libs_Predis::getInstance()->sAdd(Libs_Conf::get('live_game_key', 'redis'), $request->fd);
         Libs_Predis::getInstance()->sAdd(Libs_Conf::get('chart_game_key', 'redis'), $request->fd);
