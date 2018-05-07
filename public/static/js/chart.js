a3613fd328f5fa602ec0902cdee12b38b21b092f
chris <tanchongtao@stosz.com>
2018-05-07 17:21:18 +0800

    update

--- public/static/js/chart.js
+++ public/static/js/chart.js
@@ -1,14 +1,15 @@
 var wsServer = 'ws://127.0.0.1:9989';
 var websocket = new WebSocket(wsServer);
 websocket.onopen = function (evt) {
-    push_chart(evt.data)
+    websocket.send('hello chris 9989');
+    // push_chart(evt.data)
     console.log("Connected 9989 to WebSocket server.");
 };
 websocket.onclose = function (evt) {
     console.log("Disconnected");
 };
 websocket.onmessage = function (evt) {
-    console.log('Retrieved data from server: ' + evt.data);
+    console.log('Retrieved data from server chart : ' + evt.data);
 };
 websocket.onerror = function (evt, e) {
     console.log('Error occured: ' + evt.data);
--- public/static/js/live.js
+++ public/static/js/live.js
@@ -1,15 +1,15 @@
 var wsServer = 'ws://127.0.0.1:9988';
 var websocket = new WebSocket(wsServer);
 websocket.onopen = function (evt) {
-    websocket.send('hello chris');
-    push_live(evt.data)
-    console.log("Connected to WebSocket server.");
+    websocket.send('hello chris 9988');
+    // push_live(evt.data)
+    console.log("Connected 9988 to WebSocket server.");
 };
 websocket.onclose = function (evt) {
     console.log("Disconnected");
 };
 websocket.onmessage = function (evt) {
-    console.log('Retrieved data from server: ' + evt.data);
+    console.log('Retrieved data from server live : ' + evt.data);
 };
 websocket.onerror = function (evt, e) {
     console.log('Error occured: ' + evt.data);
