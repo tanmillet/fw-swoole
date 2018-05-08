f7f025caab38e70680ab1bab2df07dc8910f660f
chris <tanchongtao@stosz.com>
2018-05-08 09:16:34 +0800

    udpate

--- libs/libs.swws.php
+++ libs/libs.swws.php
@@ -157,6 +157,17 @@ class Libs_SwWs {
 //    [last_time] => 1525689401
 //    [close_errno] => 0
 //)
+//        reactor_id 来自哪个Reactor线程
+//server_fd 来自哪个监听端口socket，这里不是客户端连接的fd
+//server_port 来自哪个监听端口
+//remote_port 客户端连接的端口
+//remote_ip 客户端连接的IP地址
+//connect_time 客户端连接到Server的时间，单位秒，由master进程设置
+//last_time 最后一次收到数据的时间，单位秒，由master进程设置
+//close_errno 连接关闭的错误码，如果连接异常关闭，close_errno的值是非零，可以参考Linux错误信息列表
+//websocket_status [可选项] WebSocket连接状态，当服务器是Swoole\WebSocket\Server时会额外增加此项信息
+//uid [可选项] 使用bind绑定了用户ID时会额外增加此项信息
+//ssl_client_cert [可选项] 使用SSL隧道加密，并且客户端设置了证书时会额外添加此项信息
         $fdinfo = $ws->connection_info($request->fd);
         $port = isset($fdinfo['server_port']) ? $fdinfo['server_port'] : 0;
         switch ($port) {
