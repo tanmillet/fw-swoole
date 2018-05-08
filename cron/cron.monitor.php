1fe9c6246ebf2e3f5bc3a981650a413f138e6ac7
chris <tanchongtao@stosz.com>
2018-05-08 10:31:27 +0800

    update

--- cron/cron.monitor.php
+++ cron/cron.monitor.php
@@ -7,9 +7,9 @@ class Cron_Monitor {
         $shell_cmd = "netstat -anp | grep " . $port . " | grep LISTEN | wc -l";
         $res = shell_exec($shell_cmd);
         if ($res != 1) {
-            $mgs = 'start port is running :  Port [ ' . $port . ' ]  is success !';
-        } else {
             $mgs = 'start port is running :  Port [ ' . $port . ' ]  is failure !';
+        } else {
+            $mgs = 'start port is running :  Port [ ' . $port . ' ]  is success !';
         }
         echo $mgs . PHP_EOL;
     }
