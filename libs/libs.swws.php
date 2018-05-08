1209f21b26219130ec1821b64cdcbf197f68ec7e
chris <tanchongtao@stosz.com>
2018-05-08 11:25:38 +0800

    update

--- libs/libs.swws.php
+++ libs/libs.swws.php
@@ -93,30 +93,17 @@ class Libs_SwWs {
             }
         }
         //保存swoole_server 对象
-//        $this->writeLog();
-        $datas = array_merge(['date' => date("Ymd H:i:s")], $_GET, $_POST, $_SERVER);
-        $logs = "";
-        foreach ($datas as $key => $value) {
-            $logs .= $key . ":" . $value . " ";
-        }
-        print_r($logs);
-        print_r(date("d") . "_access.log");
-        swoole_async_writefile(date("d") . "_access.log", $logs . PHP_EOL, function ($filename) {
-        }, FILE_APPEND);
-
+        $this->writeLog();
         $_POST['http_server'] = $this->ws;
 
         ob_start();
-
         $action = isset($_GET['m']) ? $_GET['m'] : 'index';
         $controller = isset($_GET['c']) ? $_GET['c'] : 'home';
         $controllers = Libs_Conf::get('route_map', 'ps');
         $controller = 'Ctrs_' . (isset($controllers[$controller]) ? $controllers[$controller] : 'Home');
         (new $controller)->$action();
-
         $res = ob_get_contents();
         ob_end_clean();
-
         $response->end($res);
     }
 
@@ -154,40 +141,6 @@ class Libs_SwWs {
      */
     public function onOpen($ws, $request)
     {
-//        Array
-//        (
-//            [websocket_status] => 3
-//    [server_port] => 9988
-//    [server_fd] => 3
-//    [socket_type] => 1
-//    [remote_port] => 52902
-//    [remote_ip] => 127.0.0.1
-//    [reactor_id] => 1
-//    [connect_time] => 1525689401
-//    [last_time] => 1525689401
-//    [close_errno] => 0
-//)
-//        reactor_id 来自哪个Reactor线程
-//server_fd 来自哪个监听端口socket，这里不是客户端连接的fd
-//server_port 来自哪个监听端口
-//remote_port 客户端连接的端口
-//remote_ip 客户端连接的IP地址
-//connect_time 客户端连接到Server的时间，单位秒，由master进程设置
-//last_time 最后一次收到数据的时间，单位秒，由master进程设置
-//close_errno 连接关闭的错误码，如果连接异常关闭，close_errno的值是非零，可以参考Linux错误信息列表
-//websocket_status [可选项] WebSocket连接状态，当服务器是Swoole\WebSocket\Server时会额外增加此项信息
-//uid [可选项] 使用bind绑定了用户ID时会额外增加此项信息
-//ssl_client_cert [可选项] 使用SSL隧道加密，并且客户端设置了证书时会额外添加此项信息
-//        $fdinfo = $ws->connection_info($request->fd);
-//        $port = isset($fdinfo['server_port']) ? $fdinfo['server_port'] : 0;
-
-//        print_r($ws);
-//        foreach ($ws->ports[1]->connections as $fd) {
-//
-//            echo $fd . PHP_EOL;
-//            $_POST['http_server']->push($fd, json_encode($data));
-//        }
-//        print_r($ws);
         $fd_info = $ws->connection_info($request->fd);
         $port = isset($fd_info['server_port']) ? $fd_info['server_port'] : 0;
         switch ($port) {
@@ -236,8 +189,7 @@ class Libs_SwWs {
         foreach ($datas as $key => $value) {
             $logs .= $key . ":" . $value . " ";
         }
-
-        swoole_async_writefile(date("d") . "_access.log", $logs . PHP_EOL, function ($filename) {
+        swoole_async_writefile('../runtime/log/' . date("d") . "_access.log", $logs . PHP_EOL, function ($filename) {
 
         }, FILE_APPEND);
 
